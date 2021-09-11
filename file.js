d709bb63204d55a647a86a04049b4cd6a917c995
Dezir <j.djok9@gmail.com>
2021-09-11 15:57:04 +0300

    Создание двух функций с поднятием

---
+++ file.js
@@ -0,0 +1,10 @@
+function one (number){
+    return number*number;
+}
+function two (number) {
+    return number.toString();
+}
+let number = 111;
+console.log(number);
+console.log(one(number));
+console.log(two(number));
