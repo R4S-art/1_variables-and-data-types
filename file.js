bde81d9df8033e4b28b57168cb17315f3f2b90b0
Dezir <j.djok9@gmail.com>
2021-09-11 16:16:48 +0300

    2.Скалярные и ссылочные типы

--- file.js
+++ file.js
@@ -1,3 +1,4 @@
+//1
 function one (number){
     return number*number;
 }
@@ -8,3 +9,20 @@ let number = 111;
 console.log(number);
 console.log(one(number));
 console.log(two(number));
+//2
+function inc (n) {
+return n*n;
+}
+
+const a = 5;
+const b = inc(a);
+console.dir({a,b});
+
+const obj = {b:5}
+function INC (num){
+    num.b = num.b*num.b;
+}
+INC(obj);
+
+console.dir(obj);
+
